2f329b3ac17bdc7141b4c5d13647d5115babf390
sam jacob <sam218708@gmail.com>
2018-11-13 18:30:10 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h3 className="display-4">Hello, World!</h3>
+          <p className="lead">Boilerplate for React application developement</p>
+          <p className="lead">Includes Parcel bundler and bootstrap</p>
+          <a class="btn btn-primary btn" href="#" role="button">
+            Learn more
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
